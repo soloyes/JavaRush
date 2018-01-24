844321d0b052af9e845a6a614c291a704d7b4764
sol <solo-yes@mail.ru>
2018-01-24 16:15:15 +1000

    Task 3008 start

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/ConsoleHelper.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/ConsoleHelper.java
@@ -1,4 +1,39 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ConsoleHelper {
+    private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message){
+        System.out.println(message);
+    }
+
+    public static String readString(){
+        String message;
+            for (;;) {
+                try {
+                    message = reader.readLine();
+                    break;
+                } catch (IOException e) {
+                    System.out.println("Произошла ошибка при попытке ввода текста. Попробуйте еще раз.");
+                }
+            }
+        return message;
+    }
+
+    public static int readInt(){
+        int res;
+        for(;;) {
+            try {
+                res = Integer.parseInt(readString());
+                break;
+            } catch (NumberFormatException e){
+                System.out.println("Произошла ошибка при попытке ввода числа. Попробуйте еще раз.");
+            }
+        }
+        return res;
+    }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -1,4 +1,128 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class Server {
+
+    private static Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
+
+    public static void main(String[] args) {
+        ServerSocket serverSocket = null;
+        int port = ConsoleHelper.readInt();
+        try {
+            serverSocket = new ServerSocket(port);
+            ConsoleHelper.writeMessage("Server started");
+            for (;;){
+                Socket socket = serverSocket.accept();
+                new Handler(socket).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                serverSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void sendBroadcastMessage(Message message){
+        for (Map.Entry<String, Connection> entry:connectionMap.entrySet()) {
+            try {
+                entry.getValue().send(message);
+            } catch (IOException e) {
+                ConsoleHelper.writeMessage("Unable to send message");
+            }
+        }
+    }
+
+    private static class Handler extends Thread{
+        private Socket socket;
+
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
+
+        private String serverHandshake(Connection connection)
+                throws IOException, ClassNotFoundException {
+
+            String data = null;
+            for (;;) {
+
+                connection.send(new Message(MessageType.NAME_REQUEST));
+                Message receive = connection.receive();
+                MessageType messageType = receive.getType();
+                data = receive.getData();
+
+                if (messageType != MessageType.USER_NAME) continue;
+                if (data.isEmpty()) continue;
+                if (connectionMap.get(data) != null) continue;
+
+                connectionMap.put(data, connection);
+                connection.send(new Message(MessageType.NAME_ACCEPTED));
+                break;
+            }
+            return data;
+        }
+
+        private void sendListOfUsers(Connection connection, String userName){
+            for (Map.Entry<String, Connection> entry:connectionMap.entrySet()){
+                if (!entry.getKey().equals(userName))
+                    try {
+                        connection.send(new Message(MessageType.USER_ADDED, entry.getKey()));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+            }
+        }
+
+        private void serverMainLoop(Connection connection, String userName)
+                throws IOException, ClassNotFoundException {
+
+            for (;;){
+                Message receive = connection.receive();
+                MessageType messageType = receive.getType();
+
+                if (messageType != MessageType.TEXT) {
+                    ConsoleHelper.writeMessage("Error with message send");
+                    continue;
+                }
+
+                Message send = new Message(MessageType.TEXT, userName + ": " + receive.getData());
+                sendBroadcastMessage(send);
+            }
+        }
+
+        @Override
+        public void run() {
+            String name = null;
+            ConsoleHelper.writeMessage("Connection established with " + socket.getRemoteSocketAddress());
+
+            try (Connection connection = new Connection(socket)){
+                name = serverHandshake(connection);
+                sendListOfUsers(connection, name);
+                Message send = new Message(MessageType.USER_ADDED, name);
+                sendBroadcastMessage(send);
+
+                serverMainLoop(connection, name);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (ClassNotFoundException e){
+                e.printStackTrace();
+            }
+            finally {
+                if(name != null) connectionMap.remove(name);
+                Message send = new Message(MessageType.USER_REMOVED, name);
+                sendBroadcastMessage(send);
+            }
+
+        }
+    }
 }
